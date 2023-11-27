ec8a626ac2d5602ef4ec5555390625318e1e8f4e
Morlf1nd <l.kuchynskyy_fit_4_23_b_d@knute.edu.ua>
2023-11-28 00:34:22 +0200

    3 завдання

--- laboratorna-12.cpp
+++ laboratorna-12.cpp
@@ -1,29 +1,50 @@
 ﻿#include <iostream>
 #include <cmath>
+#include <algorithm>
 int main() {
     setlocale(0, ".1251");
-    const int n = 9;
-    int a[n] = { 0,1,1,1,4,4,7,9,2 };
+    int sum = 0;
+    int dobutok = 1;
+    const int kt = 4;
+    int array[kt];
+    for (int i = 0; i < kt; i++)
+    {
+        std::cout << "Введіть " << i << " елемент ";
+        std::cin >> array[i];
+        if (array[i] >= INT_MIN && array[i] <= INT_MAX) {
 
-    int num = 1;
-    int maxn = 1;
-
-    for (int j = 1; j < n; ++j) {
-        if (a[j] == a[j - 1]) {
-            num++;
         }
         else {
+            std::cout << "Недійсне ціле число!";
+        }
 
-            if (num > maxn) {
-                maxn = num;
-            }
-            num = 1;
+    }
+
+    for (int i = 0; i < kt; ++i) {
+        if (array[i] < 0) {
+            sum += array[i];
         }
     }
+    int max_index = 0;
+    int min_index = 0;
 
-    if (num > maxn) {
-        maxn = num;
+    for (int i = 1; i < kt; ++i) {
+        if (array[i] > array[max_index]) {
+            max_index = i;
+        }
+        if (array[i] < array[min_index]) {
+            min_index = i;
+        }
+    }
+    for (int i = min_index; i < max_index; ++i) {
+        dobutok *= array[i];
     }
 
-    std::cout << "Найбільша кількість елементів розміщених підряд: " << maxn;
+    std::cout << "Сума масиву: " << sum << std::endl;
+    std::cout << "Добуток масиву: " << dobutok << std::endl;
+
+    std::sort(array, array + kt);
+    for (int i = 0; i < kt; ++i) {
+        std::cout << array[i] << " ";
+    }
 }

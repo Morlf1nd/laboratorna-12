e91b87235d4dd7d2ae3e0bc9ed0046baa7a863ac
Morlf1nd <l.kuchynskyy_fit_4_23_b_d@knute.edu.ua>
2023-11-28 00:38:06 +0200

    4 завдання

--- laboratorna-12.cpp
+++ laboratorna-12.cpp
@@ -3,8 +3,6 @@
 #include <algorithm>
 int main() {
     setlocale(0, ".1251");
-    int sum = 0;
-    int dobutok = 1;
     const int kt = 4;
     int array[kt];
     for (int i = 0; i < kt; i++)
@@ -17,34 +15,35 @@ int main() {
         else {
             std::cout << "Недійсне ціле число!";
         }
-
     }
+    int Min = array[0];
+    for (int i = 1; i < kt; i++)
+    {
 
-    for (int i = 0; i < kt; ++i) {
-        if (array[i] < 0) {
-            sum += array[i];
+        if (fabs(array[i]) < Min)
+        {
+            Min = fabs(array[i]);
         }
     }
-    int max_index = 0;
-    int min_index = 0;
+    std::cout << "Мінімальне число в масиві за модулем це: " << Min << std::endl;
 
-    for (int i = 1; i < kt; ++i) {
-        if (array[i] > array[max_index]) {
-            max_index = i;
+    int nylik=0;
+    for (int i = 0; i < kt; i++)
+    {
+        if (array[i] == 0)
+        {
+            nylik = i;
         }
-        if (array[i] < array[min_index]) {
-            min_index = i;
-        }
-    }
-    for (int i = min_index; i < max_index; ++i) {
-        dobutok *= array[i];
     }
 
-    std::cout << "Сума масиву: " << sum << std::endl;
-    std::cout << "Добуток масиву: " << dobutok << std::endl;
 
-    std::sort(array, array + kt);
-    for (int i = 0; i < kt; ++i) {
-        std::cout << array[i] << " ";
+
+    int syma = 0;
+    for (int i = nylik; i < kt; i++)
+    {
+        syma += fabs(array[i]);
     }
+    std::cout << "Сумма модулів елементів масиву дорівнює: " << syma << std::endl;
+
+
 }

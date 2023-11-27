966f128de4a4f5a371980a613cbccacfa0aabd07
Morlf1nd <l.kuchynskyy_fit_4_23_b_d@knute.edu.ua>
2023-11-28 00:28:58 +0200

    1 завдання

---
+++ laboratorna-12.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+int main() {
+    setlocale(0, ".1251");
+    const int kt = 4;
+    int array[kt];
+    for (int i = 0; i < sizeof(array) / sizeof(array[0]); i++)
+    {
+        std::cout << "Введіть " << i << " елемент ";
+        std::cin >> array[i];
+
+    }
+
+    for (int i = 0; i < kt; ++i) {
+        if (array[i] < 0) {
+            array[i] = 0;
+        }
+        std::cout << array[i] << " ";
+    }
+}

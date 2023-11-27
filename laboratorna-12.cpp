1b1afb1dd82ece91da4ef3e7a56b8154fefce911
Morlf1nd <l.kuchynskyy_fit_4_23_b_d@knute.edu.ua>
2023-11-28 00:32:19 +0200

    2 завдання

--- laboratorna-12.cpp
+++ laboratorna-12.cpp
@@ -2,19 +2,28 @@
 #include <cmath>
 int main() {
     setlocale(0, ".1251");
-    const int kt = 4;
-    int array[kt];
-    for (int i = 0; i < sizeof(array) / sizeof(array[0]); i++)
-    {
-        std::cout << "Введіть " << i << " елемент ";
-        std::cin >> array[i];
+    const int n = 9;
+    int a[n] = { 0,1,1,1,4,4,7,9,2 };
 
-    }
+    int num = 1;
+    int maxn = 1;
 
-    for (int i = 0; i < kt; ++i) {
-        if (array[i] < 0) {
-            array[i] = 0;
+    for (int j = 1; j < n; ++j) {
+        if (a[j] == a[j - 1]) {
+            num++;
         }
-        std::cout << array[i] << " ";
+        else {
+
+            if (num > maxn) {
+                maxn = num;
+            }
+            num = 1;
+        }
     }
+
+    if (num > maxn) {
+        maxn = num;
+    }
+
+    std::cout << "Найбільша кількість елементів розміщених підряд: " << maxn;
 }
